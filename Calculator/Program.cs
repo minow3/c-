a2ce9ce8a7cdda23e31ad60f03c688a1959c82e5
Sarune Matiukaite <sharonhews@yahoo.com>
2024-01-04 12:52:22 +0000

    calculating age

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -77,6 +77,27 @@
         
     }
 
+    static void AgentAge(int userAge)
+    {
+        if (userAge < 35) {
+            Console.Write("20% extra under 35");
+        }
+        else if (userAge > 35 &&  userAge < 55)
+        {
+            Console.Write("40% extra from 35 to 55 inclusive");
+        }
+        else if (userAge > 56 && userAge < 70) 
+        {
+            Console.Write("65% etra from 56 to 70");
+        }
+        else if (userAge > 70) {
+            Console.Write("No quote provided for over 70");
+        }
+        else 
+        {
+            Console.Write("incorrect age");
+        }
+    }
 
 
 }
